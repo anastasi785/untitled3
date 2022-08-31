e716e6268def85283a02f30d8b172c04933737b4
nastu <nastushik92@bk.ru>
2022-08-31 16:18:58 +0300

    исправление дз27.08

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+        int start = 10;
+        for (start = 10; start < 0; start = -1) {
+            System.out.println(i);
+        }
+        int a = 5;
+        for (a = 5; i <= 31; i = i + 7) {
+            System.out.println("Сегодня пятница" + i + "-е число. Необходимо подготовить отчет");
+        }
+        int b = 2022 - 200;
+        int f = 2022 + 100;
+        int c = 0;
+        for (c = 0; b>f; i = i + 79) {
+            System.out.println("комета пролетит" + i + "году");
+
+        }
     }
 }
